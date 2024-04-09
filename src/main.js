7606ed79bf423ab75fac1b99047c94a6d7e288f6
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-04-09 16:04:35 -0500

    Crea la funcion que detecta la ubicación

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+const getIPlocation = async () => {
+    try {
+        const ipAPI = await fetch('http://ip-api.com/json/', { mode: 'cors' })
+		const response = await ipAPI.json()
+		return response.city
+    } catch {
+        return 'Bogotá';
+    }
+};
 
+console.log(getIPlocation())
