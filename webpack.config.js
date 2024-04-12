b84920e20ba7fc8405c1b6d2c8309eee98e0b570
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-04-12 09:30:06 -0500

    Incorpora webpack y eslint para el desarrollo

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { argv } = require('process');
+
+const [mode, modeConf] = argv.find((e) => e === 'serve')
+    ? ['development', true]
+    : ['production', false];
+
+module.exports = {
+    mode,
+    entry: './src/lib/main.js',
+
+    stats: 'none',
+
+    devtool: modeConf ? 'inline-source-map' : false,
+    devServer: {
+        static: './public',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+        // clean: !modeConf,
+        clean: true,
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(s?css)$/i,
+                use: [
+                    modeConf ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2|woff|ttf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [new CssMinimizerPlugin(), '...'],
+    },
+};
