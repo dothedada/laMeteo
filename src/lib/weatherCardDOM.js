39644120641fbc3b68a5073f6becdbb6ba3164e9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-02 22:13:35 -0500

    elimina ciclo lunar

---
+++ src/lib/weatherCardDOM.js
@@ -0,0 +1,243 @@
+const getUVtext = (uvIndex) => {
+    const uvRecomendations = [
+        'no hay radiación UV',
+        'no necesitas protección',
+        'no necesitas protección',
+        'te recomendamos usar protección',
+        'te recomendamos usar protección',
+        'te recomendamos usar protección',
+        'es necesario usar protección',
+        'es necesario usar protección',
+        'mantente a la sombra',
+        'mantente a la sombra',
+        'mantente a la sombra',
+        '¡NO SALGAS!',
+    ];
+    return `índice U.V. de ${uvIndex}, ${uvRecomendations[uvIndex]}`;
+};
+
+const getTime = () => {
+    const hour = new Date().getHours();
+    if (hour < 6) return 'noche';
+    if (hour < 8) return 'madrugada';
+    if (hour < 12) return 'mañana';
+    if (hour < 14) return 'medio día';
+    if (hour < 18) return 'tarde';
+    if (hour < 19) return 'anochecer';
+    return 'noche';
+};
+
+const airCualityText = (airCualityIndex) => {
+    const airCualityDescription = [
+        'Buena calidad del aire',
+        'Calidad del aire aceptable',
+        'Aire poco saludable para las personas más sensibles',
+        'Aire poco saludable',
+        'El aire no es saludable',
+        'El aire es tóxico',
+    ];
+    return airCualityDescription[airCualityIndex + 1];
+};
+
+const weatherDivs = (locationID, CSSclass) => {
+    const div = document.createElement('div');
+    div.setAttribute('data-id', locationID);
+    div.className = CSSclass ? `${CSSclass} ${locationID}` : locationID;
+
+    return div;
+};
+const weatherSpans = (content, CSSclass) => {
+    const span = document.createElement('span');
+    if (CSSclass) span.className = CSSclass;
+    span.textContent = content;
+
+    return span;
+};
+
+const makeWeatherCards = async (cardInfo) => {
+    if (!cardInfo) return;
+
+    const { today, tomorrow } = cardInfo;
+
+    const id = `${cardInfo.location}-${Math.floor(Math.random() * new Date().getTime()).toString(26)}`;
+
+    const localStyle = document.createElement('style');
+    localStyle.textContent = `.${id} {
+    --_img: url(${cardInfo.imageData.url});
+    --_color: black;
+    --_bk-overlay: hsl(0 0 90% / 0.6);
+}
+`;
+    document.head.appendChild(localStyle);
+
+    const location = weatherDivs(id);
+    const name = document.createElement('h2');
+    // name.className = "break"
+    name.textContent = `${cardInfo.location}\n${cardInfo.country}`;
+    const nameDescription = weatherSpans(
+        'La siguiente información corresponde a ',
+        'sr-only',
+    );
+
+    const editLocation = document.createElement('div');
+    editLocation.className = 'topRow';
+    const changeBTN = document.createElement('button');
+    changeBTN.type = 'button';
+    changeBTN.className = 'locationBTN';
+    changeBTN.textContent = 'Cambiar';
+    // changeBTN.addEventListener('click', () => {
+    //     insertPosition = `substitution_${id}`;
+    //     modal.showModal();
+    // });
+    const deleteBTN = document.createElement('button');
+    deleteBTN.type = 'button';
+    deleteBTN.textContent = 'Borrar';
+    // deleteBTN.addEventListener('click', () => {
+    //     removeWeatherDivs(id);
+    // });
+    editLocation.append(changeBTN, deleteBTN);
+
+    location.append(nameDescription, name, editLocation);
+
+    const current = weatherDivs(id, 'double');
+    current.textContent = `${today.temp.current}°`;
+
+    const dayMinMax = weatherDivs(id, 'single');
+    dayMinMax.append(
+        weatherSpans(`${today.temp.min}° min`),
+        weatherSpans(`${today.temp.max}° max`),
+    );
+
+    const currentFeel = weatherDivs(id, 'highlight');
+    currentFeel.append(
+        weatherSpans(`${today.temp.feels}°`, 'double'),
+        weatherSpans('sensación térmica', 'break'),
+    );
+
+    const currentHeat = weatherDivs(id, 'highlight');
+    currentHeat.append(
+        weatherSpans(`${today.temp.heatInd}°`, 'double'),
+        weatherSpans('índice calor', 'break'),
+    );
+
+    const currentCondition = weatherDivs(id, 'break');
+    currentCondition.textContent = today.condition;
+
+    const currentWatter = weatherDivs(id, 'single');
+    const currentRain = weatherSpans(`${today.rain}% lluvia`);
+    const currentSnow = weatherSpans(`${today.snow}% nieve`);
+    currentWatter.appendChild(currentRain);
+    if (+today.snow > 0) currentWatter.appendChild(currentSnow);
+
+    const currentAtmosphere = weatherDivs(id, 'single sectionEnd');
+    currentAtmosphere.append(
+        weatherSpans(getUVtext(+today.uv)),
+        weatherSpans(airCualityText(today.airCuality)),
+    );
+
+    const nextHourTemp = weatherDivs(id);
+    nextHourTemp.append(
+        weatherSpans('En una hora', 'topRow'),
+        weatherSpans(`${today.nextHour.temp}°`, 'double'),
+    );
+
+    const nextHourCondition = weatherDivs(id, 'break');
+    nextHourCondition.textContent = today.nextHour.condition;
+
+    const nextHourWatter = weatherDivs(id, 'single sectionEnd');
+    const nextHourRain = weatherSpans(`${today.nextHour.rain}% lluvia`);
+    const nextHourSnow = weatherSpans(`${today.nextHour.snow}% nieve`);
+    nextHourWatter.appendChild(nextHourRain);
+    if (+today.nextHour.snow > 0) nextHourWatter.appendChild(nextHourSnow);
+
+    const next2HoursTemp = weatherDivs(id);
+    next2HoursTemp.append(
+        weatherSpans('En dos horas', 'topRow'),
+        weatherSpans(`${today.next2Hours.temp}°`, 'double'),
+    );
+
+    const next2HoursCondition = weatherDivs(id, 'break');
+    next2HoursCondition.textContent = today.next2Hours.condition;
+
+    const next2HoursWatter = weatherDivs(id, 'single sectionEnd');
+    const next2HoursRain = weatherSpans(`${today.next2Hours.rain}% lluvia`);
+    const next2HoursSnow = weatherSpans(`${today.next2Hours.snow}% nieve`);
+    next2HoursWatter.appendChild(next2HoursRain);
+    if (+today.next2Hours.snow > 0)
+        next2HoursWatter.appendChild(next2HoursSnow);
+
+    const next3HoursTemp = weatherDivs(id);
+    next3HoursTemp.append(
+        weatherSpans('En tres horas', 'topRow'),
+        weatherSpans(`${today.next3Hours.temp}°`, 'double'),
+    );
+
+    const next3HoursCondition = weatherDivs(id, 'break');
+    next3HoursCondition.textContent = today.next3Hours.condition;
+
+    const next3HoursWatter = weatherDivs(id, 'single sectionEnd');
+    const next3HoursRain = weatherSpans(`${today.next3Hours.rain}% lluvia`);
+    const next3HoursSnow = weatherSpans(`${today.next3Hours.snow}% nieve`);
+    next3HoursWatter.appendChild(next3HoursRain);
+    if (+today.next3Hours.snow > 0)
+        next3HoursWatter.appendChild(next3HoursSnow);
+
+    const tomorrowTemp = weatherDivs(id);
+    tomorrowTemp.append(
+        weatherSpans('Mañana', 'topRow'),
+        weatherSpans(`${tomorrow.temp.avg}°`, 'double'),
+    );
+
+    const tomorrowMinMax = weatherDivs(id, 'single');
+    tomorrowMinMax.append(
+        weatherSpans(`${tomorrow.temp.min}° min`),
+        weatherSpans(`${tomorrow.temp.max}° max`),
+    );
+
+    const tomorrowCondition = weatherDivs(id, 'break');
+    tomorrowCondition.textContent = tomorrow.condition;
+
+    const tomorrowWatter = weatherDivs(id, 'single');
+    const tomorrowRain = weatherSpans(`${tomorrow.rain}% lluvia`);
+    const tomorrowSnow = weatherSpans(`${tomorrow.snow}% nieve`);
+    tomorrowWatter.appendChild(tomorrowRain);
+    if (+tomorrow.snow > 0) tomorrowWatter.appendChild(tomorrowSnow);
+
+    const picture = document.createElement('picture');
+    const image = document.createElement('img');
+    image.src = cardInfo.imageData.thumb;
+    image.alt = cardInfo.imageData.alt;
+    const imgBTN = document.createElement('button');
+    imgBTN.type = 'button';
+    imgBTN.className = 'zoomBTN';
+    imgBTN.textContent = 'zoom';
+    picture.append(image, imgBTN);
+
+    document.body.append(
+        location,
+        current,
+        dayMinMax,
+        currentFeel,
+        currentHeat,
+        currentCondition,
+        currentWatter,
+        currentAtmosphere,
+        nextHourTemp,
+        nextHourCondition,
+        nextHourWatter,
+        next2HoursTemp,
+        next2HoursCondition,
+        next2HoursWatter,
+        next3HoursTemp,
+        next3HoursCondition,
+        next3HoursWatter,
+        tomorrowTemp,
+        tomorrowMinMax,
+        tomorrowCondition,
+        tomorrowWatter,
+        picture,
+    );
+};
+
+export default makeWeatherCards;
+export { getTime };
---
+++ src/lib/wetherObject.js
@@ -0,0 +1,103 @@
+const makeWeatherObject = ({ createCard, info }) => {
+    if (!createCard) return { hasWeather: createCard, message: info };
+
+    const now = new Date().getHours();
+    const { current } = info;
+    const forecastDay = info.forecast.forecastday;
+
+    return {
+        hasWeather: true,
+
+        location: info.location.name,
+        region: info.location.region,
+        country: info.location.country,
+
+        today: {
+            temp: {
+                current: current.temp_c,
+                min: forecastDay[0].day.mintemp_c,
+                max: forecastDay[0].day.maxtemp_c,
+                feels: current.feelslike_c,
+                heatInd: forecastDay[0].hour[now].heatindex_c,
+            },
+            condition: current.condition.text,
+            rain: forecastDay[0].day.daily_chance_of_rain,
+            snow: forecastDay[0].day.daily_will_it_snow,
+            uv: current.uv,
+            airCuality: current.air_quality['us-epa-index'],
+            moon: forecastDay[0].astro.moon_phase,
+            moon_illumination: forecastDay[0].astro.moon_illumination,
+
+            nextHour: {
+                temp:
+                    now + 1 > 23
+                        ? forecastDay[1].hour[0].temp_c
+                        : forecastDay[0].hour[now + 1].temp_c,
+                rain:
+                    now + 1 > 23
+                        ? forecastDay[1].hour[0].chance_of_rain
+                        : forecastDay[0].hour[now + 1].chance_of_rain,
+                snow:
+                    now + 1 > 23
+                        ? forecastDay[1].hour[0].chance_of_snow
+                        : forecastDay[0].hour[now + 1].chance_of_snow,
+                condition:
+                    now + 1 > 23
+                        ? forecastDay[1].hour[0].condition.text
+                        : forecastDay[0].hour[now + 1].condition.text,
+            },
+
+            next2Hours: {
+                temp:
+                    now + 2 > 23
+                        ? forecastDay[1].hour[now + 2 - 24].temp_c
+                        : forecastDay[0].hour[now + 2].temp_c,
+                rain:
+                    now + 2 > 23
+                        ? forecastDay[1].hour[now + 2 - 24].chance_of_rain
+                        : forecastDay[0].hour[now + 2].chance_of_rain,
+                snow:
+                    now + 2 > 23
+                        ? forecastDay[1].hour[now + 2 - 24].chance_of_snow
+                        : forecastDay[0].hour[now + 2].chance_of_snow,
+                condition:
+                    now + 2 > 23
+                        ? forecastDay[1].hour[now + 2 - 24].condition.text
+                        : forecastDay[0].hour[now + 2].condition.text,
+            },
+
+            next3Hours: {
+                temp:
+                    now + 3 > 23
+                        ? forecastDay[1].hour[now + 3 - 24].temp_c
+                        : forecastDay[0].hour[now + 3].temp_c,
+                rain:
+                    now + 3 > 23
+                        ? forecastDay[1].hour[now + 3 - 24].chance_of_rain
+                        : forecastDay[0].hour[now + 3].chance_of_rain,
+                snow:
+                    now + 3 > 23
+                        ? forecastDay[1].hour[now + 3 - 24].chance_of_snow
+                        : forecastDay[0].hour[now + 3].chance_of_snow,
+                condition:
+                    now + 3 > 23
+                        ? forecastDay[1].hour[now + 3 - 24].condition.text
+                        : forecastDay[0].hour[now + 3].condition.text,
+            },
+        },
+
+        tomorrow: {
+            temp: {
+                min: forecastDay[1].day.mintemp_c,
+                max: forecastDay[1].day.maxtemp_c,
+                avg: forecastDay[1].day.avgtemp_c,
+            },
+            condition: forecastDay[1].day.condition.text,
+            rain: forecastDay[1].day.daily_chance_of_rain,
+            snow: forecastDay[1].day.daily_will_it_snow,
+            moon: forecastDay[1].astro.moon_phase,
+        },
+    };
+};
+
+export default makeWeatherObject;
